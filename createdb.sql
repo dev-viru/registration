587a86bc6e0adefbfbf1abea663584a7f0f87d8b
dev-viru <virendrachauhanmailme@gmail.com>
2023-10-19 10:09:50 +0530

    inital commit

---
+++ createdb.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `registration`.`user` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `username` VARCHAR(45) NULL,
+  `password` VARCHAR(45) NULL,
+  `email` VARCHAR(45) NULL,
+  PRIMARY KEY (`id`));
